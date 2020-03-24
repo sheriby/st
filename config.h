e078d7be9cf0ae6cf8cbdbce6deac65a6c036903
sheriby <hony_sher@foxmail.com>
2020-03-24 15:18:28 +0800

    change opacity again

--- config.h
+++ config.h
@@ -91,7 +91,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 4;
 
 /* bg opacity */
-float alpha = 0.90;
+float alpha = 0.85;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
